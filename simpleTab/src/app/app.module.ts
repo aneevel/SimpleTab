e852deef1e94c2c95a00d819b39652e37cf49295
aneevel <aneevel15@gmail.com>
2021-07-15 17:32:07 -0500

    Create view tab component

--- simpleTab/src/app/app.module.ts
+++ simpleTab/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ViewTabComponent } from './view-tab/view-tab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewTabComponent
   ],
   imports: [
     BrowserModule,
