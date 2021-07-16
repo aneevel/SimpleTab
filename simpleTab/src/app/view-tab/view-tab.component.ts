95e0e46ed224b26dc2a295fed3e0423e81015fb4
aneevel <aneevel15@gmail.com>
2021-07-15 22:21:28 -0500

    Render some sample notes with renderer

--- simpleTab/src/app/view-tab/view-tab.component.ts
+++ simpleTab/src/app/view-tab/view-tab.component.ts
@@ -9,14 +9,42 @@ import Vex from 'vexflow';
 export class ViewTabComponent implements OnInit {
 
   VF = Vex.Flow;
-  vexRenderer = this.VF.Renderer;
-
+  vexRenderer: any;
+  score: any;
+  system: any;
+  notes: any;
 
   constructor() { }
 
   ngOnInit(): void {
     let display = document.getElementById("canvas")!;
     this.vexRenderer = new this.VF.Renderer(display, this.VF.Renderer.Backends.SVG);
+
+    this.vexRenderer.resize(600, 600);
+    const context = this.vexRenderer.getContext();
+
+    this.notes = [
+      new this.VF.TabNote({
+        positions: [{str: 3, fret: 7}],
+        duration: "q"
+      }),
+
+      new this.VF.TabNote({
+        positions: [{str: 2, fret: 10},
+          {str: 3, fret: 9}],
+          duration: "q"}).
+          addModifier(new this.VF.Bend("Full"), 1),
+      
+      new this.VF.TabNote({
+        positions: [{str: 2, fret: 5}],
+        duration: "h"}). 
+        addModifier(new this.VF.Vibrato().setHarsh(true).setVibratoWidth(50), 0)
+    ];
+
+    const stave = new this.VF.TabStave(10, 40, 400);
+    stave.addClef("tab").setContext(context).draw();
+
+    this.VF.Formatter.FormatAndDraw(context, stave, this.notes);
 
   }
 
