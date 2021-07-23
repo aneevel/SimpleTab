755d60d3868a713bae6de27c4daac5e918d48e6b
aneevel <aneevel15@gmail.com>
2021-07-23 13:09:39 -0500

    Add some constants to database.php

--- simpleTab/backend/api/database.php
+++ simpleTab/backend/api/database.php
@@ -3,4 +3,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'admin');
+define('DB_NAME', 'simpletab');
 ?>
