9ee374eb94c23413ef9cf6295e3dfeaf3c444428
aneevel <aneevel15@gmail.com>
2021-07-23 13:15:01 -0500

    Add function for connecting to database

--- simpleTab/backend/api/database.php
+++ simpleTab/backend/api/database.php
@@ -7,4 +7,19 @@ define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', 'admin');
 define('DB_NAME', 'simpletab');
+
+function connect()
+{
+    $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if (mysqli_connect_errno($connect)) {
+        die("Failed to connect to database " . DB_NAME . ": " . mysqli_connect_error());
+    }
+
+    mysqli_set_charset($connect, "utf8");
+
+    return $connect;
+}
+
+$connection = connect();
 ?>
